9e4788ca9a79bea84fd02f23925d1b73b87be577
Asus <Asus@DESKTOP-O3FTUTG>
2021-03-29 12:41:01 +0300

    Добавьте файлы проекта.

---
+++ SQLiteApp/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace SQLiteApp
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("DefaultConnection") { }
+
+        public DbSet<Phone> Phones { get; set; }
+    }
+}
